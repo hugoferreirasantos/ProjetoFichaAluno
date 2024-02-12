66ddeadecf4cf1d369519f690aa5725599f9c3e8
Hugo <hugosantosadm125@gmail.com>
2024-02-12 11:02:20 -0300

    Ajuste no scrip JS na tela formulario

--- FichaAluno/FichaAluno/obj/Debug/net6.0/FichaAluno.AssemblyInfo.cs
+++ FichaAluno/FichaAluno/obj/Debug/net6.0/FichaAluno.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FichaAluno")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5cc5dc4327e0c895413dfafbee63ac85eb8a7dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("FichaAluno")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FichaAluno")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
